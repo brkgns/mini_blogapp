ea35d2a1fc046c526202cef471021f6d7f2d15a1
Berkay Güneş <gunessberkay@gmail.com>
2023-12-27 01:43:18 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:mini_blogapp/screens/homepage.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Homepage()));
+  runApp(const MaterialApp(home: Homepage(),
+  debugShowCheckedModeBanner: false,));
 }
